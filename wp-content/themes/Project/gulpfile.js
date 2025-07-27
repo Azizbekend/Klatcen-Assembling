4de9cfb3cde9c63f2f4489910f7c3eb7338a6660
azizbekktits <>
2025-07-27 12:22:59 +0300

    update/aziz/gulpfile: обновил код сборки, добавив изменения Влада

--- wp-content/themes/Project/gulpfile.js
+++ wp-content/themes/Project/gulpfile.js
@@ -20,14 +20,17 @@ const fonter = require("gulp-fonter");
 const ttf2woff = require("gulp-ttf2woff");
 const ttf2woff2 = require("gulp-ttf2woff2");
 
-const src_folder = "#src";
+// Настройки пути для WordPress
+let project_name = __dirname + "/"; // Путь к теме WordPress
+let src_folder = project_name + "#src";
+
 
 const path = {
 	build: {
-		js: "./assets/js/",
-		css: "./assets/css/",
-		images: "./assets/img/",
-		fonts: "./assets/fonts/"
+		js: project_name + "assets/js/",
+		css: project_name + "assets/css/",
+		images: project_name + "assets/img/",
+		fonts: project_name + "assets/fonts/",
 	},
 	src: {
 		js: [src_folder + "/js/app.js", src_folder + "/js/vendors.js"],
@@ -39,20 +42,30 @@ const path = {
 		js: src_folder + "/**/*.js",
 		css: src_folder + "/scss/**/*.scss",
 		images: src_folder + "/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}",
-		php: "./**/*.php"
+		php: project_name + "./**/*.php"
 	},
-	clean: "./assets/"
+	clean: project_name + "assets/"
 };
 
 // BrowserSync
 function browserSync(done) {
 	browsersync.init({
-		proxy: "http://klatcen-testing",
-		port: 3000,
-		notify: false
+		proxy: {
+			target: "http://klatcen-testing",
+		},
+		port: 3000, // Порт
+		notify: false, // Отключить уведомления
+
+		// https: true, // Включить HTTPS
+		open: 'external', // Открывать внешний URL
+		socket: {
+			domain: 'klatcen-testing:3000',
+			secure: true // Для HTTPS
+		}
 	});
 	done();
 }
+
 
 // PHP Watch (reload только)
 function phpReload(done) {
@@ -63,9 +76,16 @@ function phpReload(done) {
 // SCSS → CSS
 function css() {
 	return src(path.src.css)
-		.pipe(plumber())
-		.pipe(scss({ outputStyle: "expanded" }).on("error", scss.logError))
-		.pipe(rename({ extname: ".min.css" }))
+		.pipe(scss({ outputStyle: 'expanded' }).on('error', scss.logError))
+		.pipe(autoprefixer({
+			grid: true,
+			overrideBrowserslist: ["last 5 versions"],
+			cascade: true
+		}))
+		.pipe(group_media())
+		.pipe(dest(path.build.css))
+		.pipe(clean_css())
+		.pipe(rename({ suffix: ".min" }))
 		.pipe(dest(path.build.css))
 		.pipe(browsersync.stream());
 }
@@ -93,15 +113,17 @@ function images() {
 
 // Fonts: OTF → TTF
 function fonts_otf() {
-	return src(src_folder + "/fonts/*.otf")
+	return src(src_folder + '/fonts/*.otf', { encoding: false })
 		.pipe(plumber())
-		.pipe(fonter({ formats: ["ttf"] }))
-		.pipe(dest(src_folder + "/fonts/"));
+		.pipe(fonter({
+			formats: ['ttf']
+		}))
+		.pipe(dest(src_folder + '/fonts/'));
 }
 
 // Fonts: TTF → WOFF/WOFF2
 function fonts() {
-	src(path.src.fonts)
+	src(path.src.fonts, { encoding: false })
 		.pipe(plumber())
 		.pipe(ttf2woff())
 		.pipe(dest(path.build.fonts));
@@ -119,15 +141,40 @@ function fontstyle(done) {
 	const fileContent = fs.readFileSync(fontsFile);
 	if (fileContent === "") {
 		fs.readdir(path.build.fonts, (err, items) => {
-			if (items) {
-				let cFontname;
-				for (let i = 0; i < items.length; i++) {
-					let fontname = items[i].split(".")[0];
-					if (cFontname !== fontname) {
-						fs.appendFileSync(fontsFile, `@include font("${fontname}", "${fontname}", "400", "normal");\r\n`);
+			if (items && items.length > 0) {
+				const processedFonts = new Set();
+				let fontDeclarations = '';
+
+				items.forEach(item => {
+					const fontParts = item.split('.');
+					let fontname = fontParts[0];
+
+					if (!processedFonts.has(fontname)) {
+						let fontWeight = '400';
+						let fontStyle = 'normal';
+
+						if (fontname.toLowerCase().includes('light')) {
+							fontWeight = '300';
+						} else if (fontname.toLowerCase().includes('regular')) {
+							fontWeight = '400';
+						} else if (fontname.toLowerCase().includes('medium')) {
+							fontWeight = '500';
+						} else if (fontname.toLowerCase().includes('semibold')) {
+							fontWeight = '600';
+						} else if (fontname.toLowerCase().includes('bold')) {
+							fontWeight = '700';
+						} else if (fontname.toLowerCase().includes('italic')) {
+							fontStyle = 'italic';
+						}
+
+						fontDeclarations += `@include font("${fontname}", "${fontParts[0]}", "${fontWeight}", "${fontStyle}");\n`;
+						processedFonts.add(fontname);
 					}
-					cFontname = fontname;
-				}
+				});
+
+				fs.writeFile(fontsFilePath, fontDeclarations, cb);
+			} else {
+				console.log('No font files found in directory');
 			}
 		});
 	}
@@ -147,13 +194,99 @@ function watchFiles() {
 	watch([path.watch.php], phpReload);
 }
 
+
+function cssBuild() {
+	return src(path.src.css, {})
+		.pipe(plumber())
+		.pipe(
+			scss({ outputStyle: 'expanded' }).on('error', scss.logError)
+		)
+		.pipe(group_media())
+		.pipe(
+			autoprefixer({
+				grid: true,
+				overrideBrowserslist: ["last 5 versions"],
+				cascade: true
+			})
+		)
+		.pipe(webpcss(
+			{
+				webpClass: "._webp",
+				noWebpClass: "._no-webp"
+			}
+		))
+		.pipe(dest(path.build.css))
+		.pipe(clean_css())
+		.pipe(
+			rename({
+				extname: ".min.css"
+			})
+		)
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream());
+}
+
+
+function jsBuild() {
+	let appPath = path.build.js + 'app.min.js';
+	let vendorsPath = path.build.js + 'vendors.min.js';
+	del(appPath);
+	del(vendorsPath);
+	return src(path.src.js, {})
+		.pipe(plumber())
+		.pipe(fileinclude())
+		.pipe(dest(path.build.js))
+		.on('error', function (err) { console.log(err.toString()); this.emit('end'); })
+		.pipe(
+			rename({
+				suffix: ".min",
+				extname: ".js"
+			})
+		)
+		.pipe(dest(path.build.js))
+		.pipe(browsersync.stream());
+}
+
+function imagesBuild() {
+	return src(path.src.images, { encoding: false })
+		.pipe(newer(path.build.images))
+		.pipe(
+			imagemin([
+				webp({
+					quality: 85
+				})
+			])
+		)
+		.pipe(
+			rename({
+				extname: ".webp"
+			})
+		)
+		.pipe(dest(path.build.images))
+		.pipe(src(path.src.images, { encoding: false }))
+		.pipe(newer(path.build.images))
+		.pipe(
+			imagemin({
+				progressive: true,
+				svgoPlugins: [{ removeViewBox: false }],
+				interlaced: true,
+				optimizationLevel: 3 // 0 to 7
+			})
+		)
+		.pipe(dest(path.build.images))
+}
+
+
 // Build tasks
-const fontsBuild = series(fonts_otf, fonts, fontstyle);
-const build = series(clean, parallel(fontsBuild, css, js, images));
-const dev = series(build, parallel(watchFiles, browserSync));
+let fontsBuild = series(fonts_otf, fonts, fontstyle);
+let buildDev = series(clean, parallel(fontsBuild, css, js, images));
+let watch = series(buildDev, parallel(watchFiles, browserSync));
+let build = series(clean, parallel(cssBuild, jsBuild, imagesBuild, fontsBuild));
 
 // Export
+exports.copy = copyFolders;
 exports.fonts = fontsBuild;
-exports.clean = clean;
 exports.build = build;
-exports.default = dev;
+
+exports.watch = watch;
+exports.default = watch;
